aabda4c73ec07ba36ecf7d9ed01ed33774712697
Siddharth P <siddharthprabu74@gmail.com>
2026-04-19 03:18:33 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(0,len(nums)):
+            if target-nums[i] not in hashmap:
+                hashmap[nums[i]] = i
+            else:
+                return [hashmap[target-nums[i]],i]
